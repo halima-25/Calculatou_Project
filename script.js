b73fec63cea850c2545956a6697104b87dcdba65
Halima <h.alsheikh23@gmail.com>
2026-07-07 10:24:09 +0300

    i make some changing

--- script.js
+++ script.js
@@ -2,33 +2,44 @@ const calculatorContainer = document.getElementById('calculator_container');
 const dispalyArea = document.getElementById('dispaly_area');
 
 calculatorContainer.addEventListener('click', (e) => {
-if (e.target.nodeName = 'BUTTON') {
+if (e.target.nodeName === 'BUTTON') {
     switch (e.target.textContent){
         case 'C':
             clear();
             break;
+// we call the clear function if the pressed button was C
         case 'DEL':
             deleteOneValue();
             break;
+// we call the deleteOneValue function if the pressed button was DEL
         case '=':
             evaluate();
             break;
+// we call evaluate function if the pressed button was (=)
         default:
-           addToDisplayArea();             
+           addToDisplayArea(e.target.textContent);    
+// we call addToDisplayArea function if the pressed button was any button excpet the three buttons above
+
     }
 }
 }
 );
 
+
+
+//انشاء الدوال لاستدعائها في الاعلى
+
 function clear(){
     dispalyArea.textContent = '';
 }
+// it clears the display area 
 
 
 function addToDisplayArea(value) {
 dispalyArea.textContent = dispalyArea.textContent + value;
-
 }
+// it adds to the display area 
+
 
 function deleteOneValue(){
   let currentContent = dispalyArea.textContent;
@@ -36,6 +47,7 @@ function deleteOneValue(){
     0, currentContent.length-1
   );
 }
+
 
 function evaluate(){
     try{
